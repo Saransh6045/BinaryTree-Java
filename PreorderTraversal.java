5e190c0f9ea6f1cc0a34aaa59c37b5e7266f374a
Saransh6045 <proengineer108@gmail.com>
2025-08-25 13:41:11 +0530

    Create PreorderTraversal.java

---
+++ PreorderTraversal.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int value){
+        data = value;
+        left = null;
+        right = null;
+    }
+}
+class BinaryTree{
+    Node root;
+    
+    BinaryTree(){
+        root = null;
+    }
+    
+    void preorder(Node node){
+        if (node==null) return;
+        System.out.print(node.data + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(10);
+        tree.root.left = new Node(5);
+        tree.root.right = new Node(7);
+        tree.preorder(tree.root);
+    }
+}
